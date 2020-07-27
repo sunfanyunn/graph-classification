2e3767df5d7e779dc1c7d342d068b0741eb2358f
fanyun sun <sunfanyun@gmail.com>
2020-07-27 13:22:10 +0800

    Update go.sh

--- kernel_methods/go.sh
+++ kernel_methods/go.sh
@@ -1,21 +1,16 @@
 #!/bin/bash -ex
-#~/ENV/bin/python3 main.py gitgraph-proc wl
-#~/ENV/bin/python3 main.py gitgraph-proc graphlet
-#~/ENV/bin/python3 main.py gitgraph-proc shortest
-#~/ENV/bin/python3 main.py gitgraph-proc walk
-#~/ENV/bin/python3 main.py REDDIT-BINARY wl
 
-#~/ENV/bin/python3 main.py REDDIT-BINARY walk
-#~/ENV/bin/python3 main.py REDDIT-BINARY shortest
-#~/ENV/bin/python3 main.py REDDIT-MULTI-5K wl
-#~/ENV/bin/python3 main.py REDDIT-MULTI-5K shortest
-#~/ENV/bin/python3 main.py REDDIT-MULTI-5K walk
-#~/ENV/bin/python3 main.py IMDB-BINARY wl
-#~/ENV/bin/python3 main.py IMDB-MULTI wl
-#~/ENV/bin/python3 main.py IMDB-BINARY shortest
-#~/ENV/bin/python3 main.py IMDB-MULTI shortest
-#~/ENV/bin/python3 main.py IMDB-BINARY walk
-#~/ENV/bin/python3 main.py IMDB-MULTI walk
-#~/ENV/bin/python3 main.py REDDIT-MULTI-5K shortest
+python3 main.py REDDIT-BINARY walk
+python3 main.py REDDIT-BINARY shortest
+python3 main.py REDDIT-MULTI-5K wl
+python3 main.py REDDIT-MULTI-5K shortest
+python3 main.py REDDIT-MULTI-5K walk
+python3 main.py IMDB-BINARY wl
+python3 main.py IMDB-MULTI wl
+python3 main.py IMDB-BINARY shortest
+python3 main.py IMDB-MULTI shortest
+python3 main.py IMDB-BINARY walk
+python3 main.py IMDB-MULTI walk
+python3 main.py REDDIT-MULTI-5K shortest
 
-~/ENV/bin/python3 main.py gitgraph-proc mlg
+
