8a9a3c728673dc55ecaf5e99bdd9b40c4be5c71d
fanyun sun <sunfanyun@gmail.com>
2020-07-27 13:18:21 +0800

    Update go.sh

--- DGK/go.sh
+++ DGK/go.sh
@@ -1,18 +1,9 @@
 #!/bin/bash -ex
-DS=gitgraph-proc
-source activate my-rdkit-env2
-#python deep_kernel.py 512 3 3 $DS 5 1 1 7 100
-#python deep_kernel.py 512 3 3 $DS 5 1 1 7 100
-#python deep_kernel.py 512 3 3 $DS 5 1 1 7 100
-#python deep_kernel.py 512 3 3 $DS 5 1 1 7 100
-#python deep_kernel.py 512 3 3 $DS 5 1 1 7 100
 
-#python2 deep_kernel.py 512 3 1 $DS 5 1 1 7 100
-#python2 deep_kernel.py 512 3 1 $DS 5 1 1 7 100
-#python2 deep_kernel.py 512 3 1 $DS 5 1 1 7 100
-#python2 deep_kernel.py 512 3 1 $DS 5 1 1 7 100
-#python2 deep_kernel.py 512 3 1 $DS 5 1 1 7 100
+# Fill in the name of the dataset
+DS=
 
+# Run multiple trials 
 python2 deep_kernel.py 512 3 2 $DS 5 1 1 7 100
 python2 deep_kernel.py 512 3 2 $DS 5 1 1 7 100
 python2 deep_kernel.py 512 3 2 $DS 5 1 1 7 100
